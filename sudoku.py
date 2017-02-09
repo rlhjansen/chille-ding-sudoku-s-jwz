fec0fc71434a465112953d048bb495481e18edbe
KLSchaper <klschaper@gmail.com>
2017-02-09 19:55:38 +0100

    Start

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,14 @@
 print("hello")
 
-for i in range(3):
-    x = (i + 5)*8/4
-    print(x)
+# sudoku = [[0 for x in range(9)] for y in range(9)]
+sudo_list = []
 
+with open('puzzle1.sudoku') as file:
+    line = "_"
+    while(line != ""):
+        line = file.readline()
+        if(line != ""):
+            sudo_list.append(line)
+file.close()
+
+print(sudo_list)
