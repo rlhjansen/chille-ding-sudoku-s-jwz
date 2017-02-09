0a5b74537a0bd048ecbcadb6ab7dd30bcdbb29d9
KLSchaper <klschaper@gmail.com>
2017-02-09 19:12:55 +0100

    Wiskunde

--- sudoku.py
+++ sudoku.py
@@ -1,11 +1,6 @@
 print("hello")
 
 for i in range(3):
-    print(i)
-    print("JA")
-    print("NEE")
-    print("urmom")
-    print("harambea")
-    print("Can't stop the trump-train")
-    print("choo choo")
-    break
+    x = (i + 5)*8/4
+    print(x)
+
