6eeb70fa8f9e0b65eee1f7afc7f72978eab739b7
rlhjansen <rlh.jansen@outlook.com>
2017-02-11 17:16:33 +0100

    eigen probeersels, veel plezier ermee

--- reitzeversie.py
+++ reitzeversie.py
@@ -37,17 +37,18 @@ def changeToNeeds(List):
             complete.remove(List[i])
     return complete
 
-blockNumberRange = {
-    0: [[0, 1, 2], [0, 1, 2]],
-    1: [[3, 4, 5], [0, 1, 2]],
-    2: [[6, 7, 8], [0, 1, 2]],
-    3: [[0, 1, 2], [3, 4, 5]],
-    4: [[3, 4, 5], [3, 4, 5]],
-    5: [[6, 7, 8], [3, 4, 5]],
-    6: [[0, 1, 2], [6, 7, 8]],
-    7: [[3, 4, 5], [6, 7, 8]],
-    8: [[6, 7, 8], [6, 7, 8]]
-}
+blockNumberRange = [
+    [[0, 1, 2], [0, 1, 2]],
+    [[3, 4, 5], [0, 1, 2]],
+    [[6, 7, 8], [0, 1, 2]],
+    [[0, 1, 2], [3, 4, 5]],
+    [[3, 4, 5], [3, 4, 5]],
+    [[6, 7, 8], [3, 4, 5]],
+    [[0, 1, 2], [6, 7, 8]],
+    [[3, 4, 5], [6, 7, 8]],
+    [[6, 7, 8], [6, 7, 8]]
+]
+
 
 def constructBlock(Coordinates):
     block = []
@@ -77,7 +78,29 @@ for j in range(len(sudo_blocks)):
     sudo_blocks_needs.append(changeToNeeds(sudo_blocks[j]))
 
 
-print(positionNeeds([2,4],[1,2,3,4],[2,3,4]))
+# This part makes a new row list of the sudoku.
+# If we can get this to repeat until sudo_rows_need is comprised of empty
+# lists we can solve the easy sudoku's presumably, i cant seem to figure out the while loop for it though
+
+
+new_version_rows = []
+for j in range(len(sudo_rows)):
+    row = []
+    for i in range(len(sudo_rows[0])):
+        if sudo_rows[j][i] != 0:
+            row.append(sudo_rows[j][i])
+        else:
+            f = 0
+            for k in range(len(blockNumberRange)):
+                if (j in blockNumberRange[1]) & (i in blockNumberRange[0]):
+                    f = k
+            possibilities = positionNeeds(sudo_rows_needs[j], sudo_columns_needs[i], sudo_blocks_needs[f])
+            possibility_number = len(positionNeeds(sudo_rows_needs[j], sudo_columns_needs[i], sudo_blocks_needs[f]))
+            if(possibility_number<2)&(possibility_number>0):
+                row.append(possibilities[0])
+            else:
+                row.append(sudo_rows[j][i])
+    new_version_rows.append(row)
 
 print(sudo_rows)
 print(sudo_columns)
@@ -85,4 +108,4 @@ print(sudo_blocks)
 print(sudo_rows_needs)
 print(sudo_columns_needs)
 print(sudo_blocks_needs)
-
+print(new_version_rows)
