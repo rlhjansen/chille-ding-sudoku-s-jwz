b8705517b98e1617210fbc056002a6c7b1cc1efa
KLSchaper <klschaper@gmail.com>
2017-02-20 19:53:52 +0100

    Matrix

--- sudoku.py
+++ sudoku.py
@@ -5,9 +5,9 @@ sudo_list = []
 
 with open('puzzle1.sudoku') as file:
     line = "_"
-    while(line != ""):
+    while line != "" :
         line = file.readline()
-        if(line != ""):
+        if line != "" :
             row = line.split(",")
             for i in range(len(row)):
                 row[i] = int(row[i][0])
