42fb4c9c91b4567a1613226ae40314df6bf9b217
Justaddice <jochem.holscher@gmail.com>
2017-03-16 20:44:52 +0100

    Made the move function in chip_functions.py

---
+++ Circuit_Chipper/Chip_Functions.py
@@ -0,0 +1,91 @@
+# This file contains a list of functions shared by most other files.
+
+length = 0
+
+
+#
+def move_north(grid, position, wire):
+    global length
+    x = position[2]
+    y = position[1] + 1
+    z = position[0]
+
+    grid[z][y][x] = wire
+    length += 1
+    return [z, y, x]
+
+
+#
+def move_east(grid, position, wire):
+    global length
+    x = position[2] + 1
+    y = position[1]
+    z = position[0]
+
+    grid[z][y][x] = wire
+    length += 1
+    return [z, y, x]
+
+
+#
+def move_south(grid, position, wire):
+    global length
+    x = position[2]
+    y = position[1] - 1
+    z = position[0]
+
+    grid[z][y][x] = wire
+    length += 1
+    return [z, y, x]
+
+
+#
+def move_west(grid, position, wire):
+    global length
+    x = position[2] - 1
+    y = position[1]
+    z = position[0]
+
+    grid[z][y][x] = wire
+    length += 1
+    return [z, y, x]
+
+
+#
+def move_up(grid, position, wire):
+    global length
+    x = position[2]
+    y = position[1]
+    z = position[0] + 1
+
+    grid[z][y][x] = wire
+    length += 1
+    return [z, y, x]
+
+
+#
+def move_down(grid, position, wire):
+    global length
+    x = position[2]
+    y = position[1]
+    z = position[0] - 1
+
+    grid[z][y][x] = wire
+    length += 1
+    return [z, y, x]
+
+
+#
+def move(grid, direction, position, wire):
+    if direction == 'north':
+        return move_north(grid, position, wire)
+    elif direction == 'east':
+        return move_east(grid, position, wire)
+    elif direction == 'south':
+        return move_south(grid, position, wire)
+    elif direction == 'west':
+        return move_west(grid, position, wire)
+    elif direction == 'up':
+        return move_up(grid, position, wire)
+    elif direction == 'down':
+        return move_down(grid, position, wire)
--- Circuit_Chipper/Search_Algorithms.py
+++ Circuit_Chipper/Search_Algorithms.py
@@ -1,3 +1,5 @@
 # This file contains various search algorithms, they will be compared on
 # speed and performance. Programs that import this class will gain access
 # to all these algorithms.
+
+
