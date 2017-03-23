bf265eaa26743a9f132825064b732b3d55001d6c
Justaddice <jochem.holscher@gmail.com>
2017-03-23 21:03:00 +0100

    Logboek bijgwerkt en een find_conflicts geschreven.

--- Circuit_Chipper/Chip_Functions.py
+++ Circuit_Chipper/Chip_Functions.py
@@ -10,7 +10,7 @@ def move_north(grid, position, wire):
     y = position[1] - 1
     z = position[0]
 
-    grid[z][y][x] = grid[z][y][x] + wire
+    grid[z][y][x] = wire
     length += 1
     return [z, y, x]
 
@@ -22,7 +22,7 @@ def move_east(grid, position, wire):
     y = position[1]
     z = position[0]
 
-    grid[z][y][x] = grid[z][y][x] + wire
+    grid[z][y][x] = wire
     length += 1
     return [z, y, x]
 
@@ -34,7 +34,7 @@ def move_south(grid, position, wire):
     y = position[1] + 1
     z = position[0]
 
-    grid[z][y][x] = grid[z][y][x] + wire
+    grid[z][y][x] = wire
     length += 1
     return [z, y, x]
 
@@ -46,7 +46,7 @@ def move_west(grid, position, wire):
     y = position[1]
     z = position[0]
 
-    grid[z][y][x] = grid[z][y][x] + wire
+    grid[z][y][x] = wire
     length += 1
     return [z, y, x]
 
@@ -58,7 +58,7 @@ def move_up(grid, position, wire):
     y = position[1]
     z = position[0] + 1
 
-    grid[z][y][x] = grid[z][y][x] + wire
+    grid[z][y][x] = wire
     length += 1
     return [z, y, x]
 
@@ -70,7 +70,7 @@ def move_down(grid, position, wire):
     y = position[1]
     z = position[0] - 1
 
-    grid[z][y][x] = grid[z][y][x] + wire
+    grid[z][y][x] = wire
     length += 1
     return [z, y, x]
 
@@ -91,16 +91,112 @@ def move(grid, direction, position, wire):
         return move_down(grid, position, wire)
 
 
-# Scan through the grid and return the coordinates of all existing conflicts.
-def find_conflicts(grid):
-    conflicts = []
+# Remove
+def remove_north(grid, position):
+    global length
+    x = position[2]
+    y = position[1]
+    z = position[0]
+
+    grid[z][y][x] = ''
+    length -= 1
+    return [z, y - 1, x]
+
+
+# Move one east, then append the name of the wire to that place.
+def remove_east(grid, position):
+    global length
+    x = position[2]
+    y = position[1]
+    z = position[0]
+
+    grid[z][y][x] = ''
+    length -= 1
+    return [z, y, x - 1]
+
+
+# Move one south, then append the name of the wire.
+def remove_south(grid, position):
+    global length
+    x = position[2]
+    y = position[1] + 1
+    z = position[0]
+
+    grid[z][y][x] = ''
+    length -= 1
+    return [z, y - 1, x]
+
+
+# Move west, then append the name of the wire.
+def remove_west(grid, position):
+    global length
+    x = position[2]
+    y = position[1]
+    z = position[0]
+
+    grid[z][y][x] = ''
+    length -= 1
+    return [z, y, x + 1]
+
+
+# Move down, delete the name of the wire.
+def remove_up(grid, position):
+    global length
+    x = position[2]
+    y = position[1]
+    z = position[0]
+
+    grid[z][y][x] = ''
+    length -= 1
+    return [z - 1, y, x]
+
+
+# Move up, delete the wire.
+def remove_down(grid, position):
+    global length
+    x = position[2]
+    y = position[1]
+    z = position[0]
+
+    grid[z][y][x] = ''
+    length -= 1
+    return [z + 1, y, x]
+
+
+# Determine which direction you need to move.
+def remove(grid, direction, position):
+    if direction == 'north':
+        return remove_north(grid, position)
+    elif direction == 'east':
+        return remove_east(grid, position)
+    elif direction == 'south':
+        return remove_south(grid, position)
+    elif direction == 'west':
+        return remove_west(grid, position)
+    elif direction == 'up':
+        return remove_up(grid, position)
+    elif direction == 'down':
+        return remove_down(grid, position)
+
+
+# Find for a point with the name
+def get_gate(grid, gate_num):
+    gate_name = 'G' + str(gate_num)
 
     for z in range(len(grid)):
         for y in range(len(grid[z])):
             for x in range(len(grid[z][y])):
                 point = grid[z][y][x]
 
-                if point != '' and not point[1:].isdecimal():
-                    conflicts.append([z, y, x])
+                if point == gate_name:
+                    return [z, y, x]
 
-    return conflicts
+    return None
+
+
+#
+def lay_wire(grid, start_gate, end_gate):
+    start_gate = get_gate(start_gate)
+    end_gate = get_gate(end_gate)
+
+
