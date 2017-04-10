37c4e625dc243ef0da35bef7898d918a9e6ac52b
Justaddice <jochem.holscher@gmail.com>
2017-04-10 12:04:19 +0200

    Oh baby, bug uit de wire_class gehaald, een hillclimber geschreven met wat hulpmethods.
    Hij vindt nu al oplossingen! En ze zijn maar een heel klein beetje bugged!
    \o/

--- Circuit_Chipper/Search_Algorithms.py
+++ Circuit_Chipper/Search_Algorithms.py
@@ -1,6 +1,7 @@
 # This file contains various search algorithms, they will be compared on
 # speed and performance. Programs that import this class will gain access
 # to all these algorithms.
+from random import randint
 
 
 # print the chip.
@@ -21,11 +22,13 @@ class Wire:
     num_wires = 0
     wires_layed = 0
 
-    def __init__(self, grid, coordinates):
+    def __init__(self, grid, coordinates, start, end):
         self.name = 'W' + str(Wire.num_wires)
         self.coordinates = coordinates
         Wire.lay(self, grid, coordinates)
         self.conflicts = Wire.num_conflicts(self, grid)
+        self.start = start
+        self.end = end
 
     def remove(self, grid):
         Wire.num_wires -= 1
@@ -35,9 +38,10 @@ class Wire:
             y = coordinate[1]
             z = coordinate[0]
 
-            grid[z][y][x].replace(self.name, '')
+            grid[z][y][x] = grid[z][y][x].replace(self.name, '')
             Wire.wire_length -= 1
-            self.coordinates = []
+
+        self.coordinates = []
 
     def lay(self, grid, coordinate):
         self.coordinates = coordinate
@@ -88,6 +92,24 @@ def legal_position(position, grid, end):
     return False
 
 
+# Returns the amount of overlapping wires/gates in the grid.
+def num_conflicts(grid):
+    conflicts = 0
+    for layer in grid:
+        for row in layer:
+            for node in row:
+                num_wires = 0
+
+                for character in node:
+                    if character.isalpha():
+                        num_wires += 1
+
+                if num_wires > 1:
+                    conflicts += num_wires -2
+
+    return conflicts
+
+
 # Draw a wire from start to end with the A-star method.
 def connect_wire(start, end, grid):
 
@@ -111,13 +133,10 @@ def connect_wire(start, end, grid):
             heuristik = len(path) - 1 + distance_heuristik(pointer, end)
             new_path = [heuristik] + path[1:] + [tuple(pointer)]
 
-            if False == True:
-                check += 1
-                if check % 100 == 0:
-                    print("check")
-                    new_wire = Wire(grid, new_path[2:-1])
-                    print_grid(grid)
-                    Wire.remove(new_wire, grid)
+            check += 1
+            if check == 400:
+                return False
+
 
             index = 0
             while index < len(paths) and paths[index][0] >= heuristik:
@@ -131,9 +150,63 @@ def connect_wire(start, end, grid):
         if len(paths) == 0:
             return False
 
-    print("Found path:", paths[-1][2:-1])
     return paths[-1][2:-1]
 
 
+#
+def choose_wires(num, wires):
+    wires_chosen = []
+
+    while len(wires_chosen) < num:
+        index = randint(1, len(wires)) - 1
+
+        if index not in wires_chosen:
+            wires_chosen.append(wires[index])
+
+    return wires_chosen
+
+
+#
+def revert_mutation(grid, wires, coordinates):
+    for index in range(len(wires)):
+        Wire.remove(wires[index], grid)
+        Wire.lay(wires[index], grid, coordinates[index])
+
+
+#
+def mutate_wires(grid, wires):
+    old_coordinates = []
+
+    for wire in wires:
+        old_coordinates.append(wire.coordinates)
+        Wire.remove(wire, grid)
+
+    for wire in wires:
+        path = connect_wire(wire.start, wire.end, grid)
+
+        if path == False:
+            revert_mutation(grid, wires, old_coordinates)
+            return False
+
+        Wire.lay(wire, grid, path)
+
+    return old_coordinates
+
+
+#
+def hillclimber(grid, wires):
+    conflicts = num_conflicts(grid)
+
+    while conflicts > 0:
+        wires_chosen = choose_wires(2, wires)
+        wire_backup = mutate_wires(grid, wires_chosen)
+
+        if wire_backup:
+            new_conflicts = num_conflicts(grid)
+
+            if new_conflicts > conflicts:
+                revert_mutation(grid, wires_chosen, wire_backup)
+            else:
+                conflicts = new_conflicts
 
 
